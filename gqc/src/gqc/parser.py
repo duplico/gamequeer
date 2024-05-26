45778631f1e5c62c341edf1c946ef4508b47086b
George Louthan <duplico@dupli.co>
2024-05-26 22:39:07 +0000

    Modest grammar update.

--- gqc/src/gqc/parser.py
+++ gqc/src/gqc/parser.py
@@ -13,7 +13,12 @@ var_definition = string_definition | int_definition
 string_definition = "str" identifier ":=" string ";" # | "str" identifier ";"
 int_definition = "int" identifier "=" integer ";" # | "int" identifier ";"
 
-animation_definition_section = "animations" file_assignments
+animation_definition_section = "animations" animation_assignments
+animation_assignments = animation_assignment | "{" animation_assignment* "}"
+animation_assignment = identifier <-:" file_source ";" | identifier <-:" file_source animation_options
+animation_options = animation_option | "{" animation_option* "}"
+animation_option = "frame_rate" "=" integer ";" | "dithering" ":=" string ";"
+
 lightcue_definition_section = "lightcues" file_assignments
 
 file_assignments = file_assignment | "{" file_assignment* "}"
@@ -22,7 +27,7 @@ file_source = STRING
 
 menu_definition_section = "menus" menu_definitions
 menu_definitions = menu_definition | "{" menu_definition* "}"
-menu_definition = "menu" identifier menu_options
+menu_definition = identifier menu_options
 menu_options = menu_option | "{" menu_option* "}"
 menu_option = INT ":" STRING ";"
 
@@ -46,7 +51,7 @@ program = pp.Forward()
 # Basic tokens
 identifier = pp.Word(pp.alphas, pp.alphanums + "_").setName("identifier")
 string = pp.QuotedString('"').setName("string")
-integer = pp.Word(pp.nums).setName("integer")
+integer = pp.Word(pp.nums).setName("integer").setParseAction(lambda t: int(t[0]))
 
 int_type = pp.Keyword("int").setName("int")
 str_type = pp.Keyword("str").setName("str")
@@ -58,17 +63,25 @@ var_definition = int_definition | string_definition
 var_definitions = var_definition | pp.Suppress("{") - pp.ZeroOrMore(var_definition) - pp.Suppress("}")
 var_definition_section = pp.Group((pp.Keyword("volatile") | pp.Keyword("persistent")) - pp.Group(var_definitions))
 
-# Animation and lightcue sections
+# File assignments for animations and lightcues
 file_source = string
 file_assignment = pp.Group(identifier - pp.Suppress("<-") - file_source - pp.Suppress(";"))
 file_assignments = pp.Group(file_assignment | pp.Suppress("{") - pp.ZeroOrMore(file_assignment) - pp.Suppress("}"))
-animation_definition_section = pp.Group(pp.Keyword("animations") - file_assignments)
+
+# Animation sections
+animation_option = pp.Group(pp.Keyword("frame_rate") - pp.Suppress("=") - integer - pp.Suppress(";") | pp.Keyword("dithering") - pp.Suppress(":=") - string - pp.Suppress(";"))
+animation_options = pp.Group(pp.Suppress(";") | animation_option | pp.Suppress("{") - pp.ZeroOrMore(animation_option) - pp.Suppress("}"))
+animation_assignment = pp.Group(identifier - pp.Suppress("<-") - file_source - animation_options)
+animation_assignments = pp.Group(animation_assignment | pp.Suppress("{") - pp.ZeroOrMore(animation_assignment) - pp.Suppress("}"))
+animation_definition_section = pp.Group(pp.Keyword("animations") - animation_assignments)
+
+# Light cue sections
 lightcue_definition_section = pp.Group(pp.Keyword("lightcues") - file_assignments)
 
 # # Menu sections
 menu_option = pp.Group(integer - pp.Suppress(":") - string - pp.Suppress(";"))
 menu_options = pp.Group(menu_option | pp.Suppress("{") - pp.ZeroOrMore(menu_option) - pp.Suppress("}"))
-menu_definition = pp.Group(pp.Keyword("menu") - identifier - menu_options)
+menu_definition = pp.Group(identifier - menu_options)
 menu_definitions = pp.Group(menu_definition | pp.Suppress("{") - pp.ZeroOrMore(menu_definition) - pp.Suppress("}"))
 menu_definition_section = pp.Group(pp.Keyword("menus") - menu_definitions)
 
