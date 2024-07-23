402e6c9e0a3a944f264f40327d6b4ce3c13bf2a5
George <duplico@dupli.co>
2024-07-23 03:00:29 +0000

    Handle single-frame animations

--- gqc/src/gqc/anim.py
+++ gqc/src/gqc/anim.py
@@ -1,21 +1,12 @@
 import ffmpeg
 import pathlib
 
+from PIL import Image
+from PIL.Image import Dither
+
 from rich.progress import Progress
 
-def make_animation(progress: Progress, anim_src_path : pathlib.Path, output_dir : pathlib.Path, dithering : str = 'none', frame_rate : int = 25, height : int = -1, width : int = -1):
-    # Set up the output directory
-    output_dir.mkdir(parents=True, exist_ok=True)
-    # Delete the old output files: anim.gif and frame*.bmp
-    for file in output_dir.glob('anim.gif'):
-        file.unlink()
-    for file in output_dir.glob('frame*.bmp'):
-        file.unlink()
-
-    # Check whether the source file exists and raise a value error if it doesn't
-    if not anim_src_path.exists():
-        raise ValueError(f"Animation source file {anim_src_path} does not exist")
-
+def make_animation_from_video(progress: Progress, anim_src_path : pathlib.Path, output_dir : pathlib.Path, dithering : str = 'none', frame_rate : int = 25, height : int = 128, width : int = 128):
     # Load the source file
     in_file = ffmpeg.input(anim_src_path)
 
@@ -46,6 +37,57 @@ def make_animation(progress: Progress, anim_src_path : pathlib.Path, output_dir 
         except ffmpeg._run.Error as e:
             raise ValueError(f"ffmpeg error; Raw output of ffmpeg follows: \n\n{e.stderr.decode()}")
         progress.update(task, advance=1)
+
+def make_animation_from_image(progress: Progress, anim_src_path : pathlib.Path, output_dir : pathlib.Path, dithering : str = 'none', duration : int = 100, height : int = 128, width : int = 128):
+    # Load the source file
+    in_img = Image.open(anim_src_path)
+
+    if dithering == 'none':
+        dither_method = Dither.NONE
+    elif dithering == 'floyd_steinberg':
+        dither_method = Dither.FLOYDSTEINBERG
+    else:
+        raise ValueError(f"Dithering algorithm {dithering} is not supported for single images.")
+
+    # Scale the image to make its height or width the specified size
+    scaled = in_img.resize((width, height))
+    # Quantize the image to black and white, using the specified dithering algorithm
+    quantized = scaled.convert('1', dither=dither_method)
+
+    # Write the output - summary gif and frame files
+    out_files = [
+        (output_dir / 'anim.gif', progress.add_task(f" [dim]-> gif summary", total=1, start=False)),
+        (output_dir / 'frame0000.bmp', progress.add_task(f" [dim]-> bmp frames", total=1, start=False))
+    ]
+
+    for out_file, task in out_files:
+        try:
+            progress.start_task(task)
+            quantized.save(out_file)
+            progress.update(task, completed=1, total=1)
+        except OSError as e:
+            raise ValueError(f"Error writing image; {e}")
+
+def make_animation(progress: Progress, anim_src_path : pathlib.Path, output_dir : pathlib.Path, dithering : str = 'none', frame_rate : int = 25, duration : int = 100, height : int = 128, width : int = 128):
+    # Set up the output directory
+    output_dir.mkdir(parents=True, exist_ok=True)
+    # Delete the old output files: anim.gif and frame*.bmp
+    for file in output_dir.glob('anim.gif'):
+        file.unlink()
+    for file in output_dir.glob('frame*.bmp'):
+        file.unlink()
+
+    # Check whether the source file exists and raise a value error if it doesn't
+    if not anim_src_path.exists():
+        raise ValueError(f"Animation source file {anim_src_path} does not exist")
+    
+    # Check whether the source file is a video or an image
+    if anim_src_path.suffix in ['.bmp', '.png', '.jpg', '.jpeg']:
+        make_animation_from_image(progress, anim_src_path, output_dir, dithering, duration, height, width)
+    else:
+        make_animation_from_video(progress, anim_src_path, output_dir, dithering, frame_rate, height, width)
+
+    
 
 def convert_animations(assets_dir : pathlib.Path, build_dir : pathlib.Path):
     pass
