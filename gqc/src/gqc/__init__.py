07834b86181213bfc0de1ec0fe13b3022e516d75
George <duplico@dupli.co>
2024-07-24 05:27:49 +0000

    Fix missing import

--- gqc/src/gqc/__init__.py
+++ gqc/src/gqc/__init__.py
@@ -1,5 +1,7 @@
 __version__ = '0.1.0'
 
+import pyparsing as pp
+
 class GqcParseError(Exception):
     def __init__(self, message, s, loc):
         message = f"Error at line {pp.lineno(loc, s)}, column {pp.col(loc, s)}: {message}"
