0d30ec0d267f9d97384dd70601159c13d7aaeb48
George <duplico@dupli.co>
2024-08-07 04:09:24 -0500

    Re-tick in the system

--- gamequeer/src/gamequeer.c
+++ gamequeer/src/gamequeer.c
@@ -318,7 +318,7 @@ void system_tick() {
     // Should be called by the 100 Hz system tick
 
     // Handle the LEDs
-//    led_tick();
+    led_tick();
 
     // Handle timers
     if (timer_active) {
